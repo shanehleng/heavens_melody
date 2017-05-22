bdb1f1a1000492d2fef00a74107eeba8b1fe7fd2
Shaneh Leng <shashanileigh@gmail.com>
2017-05-22 11:32:06 +0100

    initial commit for heaven's melody files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function main() {
+    (function () {
+        'use strict';
+
+        /* Show Menu on Book */
+        $(window).bind('scroll', function () {
+            var navHeight = $(window).height() - 600;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        $(":file").filestyle();
+
+        /* Collapsing Navigation Bar */
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        });
+
+    }());
+}
+         
+main();
